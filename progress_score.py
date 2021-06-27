a8a0339553a1ecf47c32b163080c9b23337258b5
Joe Bainbridge (UG) <b9020997@newcastle.ac.uk>
2021-06-27 19:38:15 +0100

    Implemented tests for 'calculate_progress_score' function

--- progress_score.py
+++ progress_score.py
@@ -1,2 +1,2 @@
-def calculate_progress_score():
+def calculate_progress_score(mock_result, actual_result):
     return 0
--- test_script.py
+++ test_script.py
@@ -4,7 +4,29 @@ from random import randint
 
 
 def test_progress_score():
-    assert calculate_progress_score() == 0
+    # example test cases
+    assert calculate_progress_score(80, 75) == -0.5
+    assert calculate_progress_score(57, 65) == -0.8
+    assert calculate_progress_score(50, 80) == 3
+    assert calculate_progress_score(99, 88) == -1.1
+    # edge cases
+    assert calculate_progress_score(100, 100) == 0
+    assert calculate_progress_score(0, 0) == 0
+    # near edge cases
+    assert calculate_progress_score(99, 99) == 0
+    assert calculate_progress_score(1, 1) == 0
+    # near edge cases different values - negative
+    assert calculate_progress_score(99, 96) == -0.3
+    assert calculate_progress_score(3, 1) == -0.2
+    # near edge cases different values - positive
+    assert calculate_progress_score(96, 99) == 0.3
+    assert calculate_progress_score(1, 3) == 0.2
+    # random values
+    for x in range(100):
+        val1 = randint(0, 100)
+        val2 = randint(0, 100)
+        expected_val = round(((val2-val1)/10), 1)
+        assert calculate_progress_score(val1, val2) == expected_val
 
 
 def test_average_test_score():
